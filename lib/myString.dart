39659035d5146b284878a552c8239398298f80bf
kiarash-sh81 <kiarashshahroudi@gmail.com>
2024-03-29 00:06:55 +0330

    some constant added

---
+++ lib/myString.dart
@@ -0,0 +1,7 @@
+class myString {
+  static const String viewHotBlog = "مشاهده داغ ترین نوشته ها ";
+  static const String viewHotPodCast = "مشاهده داغ ترین پادکست ها ";
+  static const String relatedArticel = "نوشته های مرتبط ";
+  static const String submitArticel = "ارسال مقاله";
+  static const String submitPodcast = "ارسال پادکست ";
+}
---
+++ lib/mycolors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class solidColor {
+  static const Color colorTitle = Color.fromARGB(255, 40, 107, 184);
+  static const Color textTitle = Color.fromARGB(255, 0, 0, 0);
+  static const Color scafoldBg = Color.fromARGB(255, 255, 255, 255);
+}
+
+class gradiantColor {
+  static const List<Color> bottomNav = [const Color.fromARGB(255, 25, 0, 94) ,const Color.fromARGB(255, 68, 4, 78) ]; 
+}
